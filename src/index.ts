f33c8749a32a6d5b219cc8389a9f09d7e0ada0fb
Chris Scribner <scriby@gmail.com>
2018-11-13 23:11:32 -0800

    Add LRUCache types to index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export {IMap, ISingleValueNode, Key, MapAdapter} from './map';
 export {ISortedCollection, LookupNodeInfo, ParentPath, SortedCollectionAdapter} from './sortedcollection';
 export {GetOrderingKey, IKeyWithOrder, ISortedMap, SortedMapAdapter} from './sortedmap';
+export {LruCacheAdapter, ILruCache} from './lrucache';
